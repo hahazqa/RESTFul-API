5479871cbffa5027420f285e1334a0993cc5e968
hahazqa <542083304@qq.com>
2019-07-30 16:56:10 +0800

    test

--- index.js
+++ index.js
@@ -2,6 +2,6 @@ const Koa = require('koa');
 const app = new Koa();
 
 app.use((ctx)=>{
-    ctx.body = "hello world hahaha xixixi"
+    ctx.body = "hello world hahaha xixixif"
 });
 app.listen(3000);
